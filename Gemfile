0dec667ee453d4d6d4b4b5048d4eace5321acf00
Kyle Conarro <kyle.conarro@rigor.com>
2013-12-04 15:07:06 -0500

    trying to add coveralls

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source "http://rubygems.org"
 
+gem 'coveralls', require: false
 # Specify your gem's dependencies in har.gemspec
 gemspec
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,6 +15,9 @@ Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 $LOAD_PATH.unshift File.expand_path("../lib", __FILE__)
 require 'har'
 require 'pry'
+require 'coveralls'
+
+Coveralls.wear!
 
 module HAR
   module SpecHelper
